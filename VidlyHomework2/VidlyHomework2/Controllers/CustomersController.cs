fd51801a02e25e742c184db61dca9e314dd6123d
Zarek Chung <simen1127@gmail.com>
2017-08-10 17:36:14 +0800

    HW2

---
+++ VidlyHomework2/VidlyHomework2/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyHomework2.Models;
+
+namespace VidlyHomework2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+
+            Customer customer = new Customer
+            {
+                Name = "Zarek Chung"
+            };
+            return View();
+        }
+    }
+}
---
+++ VidlyHomework2/VidlyHomework2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VidlyHomework2.MvcApplication" Language="C#" %>

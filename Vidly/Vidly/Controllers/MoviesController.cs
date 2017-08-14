799e046342810fa3433404a1bf17b0b090a787b3
Zarek Chung <simen1127@gmail.com>
2017-08-14 16:55:25 +0800

    Vidly ch2- ch3

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Moive/Random
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Zarek!!" };
+
+            //ViewModel
+            var customers = new List<Customer>
+            {
+                new Customer { Name= "customer 1"},
+                new Customer { Name = "customer 2" }
+
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //return type
+            //return RedirectToAction("Index", "Home", new {page = 1, sortby="name"});
+            //return new EmptyResult();
+            //return HttpNotFound();
+            //return Content("Hello World!");
+
+            //ViewDate and ViewBag
+            ViewData["RandomMovie"] = movie;
+            ViewBag.RandomMovie = movie;
+
+            return View(viewModel);
+        }
+
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace((sortBy)))
+                sortBy = "name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
---
+++ VidlyHw02/VidlyHw02/Controllers/CustomersController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyHw02.Models;
+
+namespace VidlyHw02.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context= new ApplicationDbContext();
+            ;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            //var customers = _context
+            return View(customers);
+
+        }
+        public ActionResult Details(int Id)
+        {
+
+            var customers = GetCustomers().SingleOrDefault(c => c.Id == Id);
+
+
+            //var customers = tmp.SingleOrDefault(c => c.Id == Id);
+
+            if(customers == null)
+                return HttpNotFound();
+            return View(customers);
+        }
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Name= "Zarek Chung", Id = 1},
+                new Customer { Name = "Simen Chung" ,Id = 2}
+            };
+        }
+    }
+}
